593a20322adb4a57c718d141a1ea21e06e6ff786
shoaib memon <shoaibattari83@gmail.com>
2023-10-28 15:21:46 +0500

    courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -15,7 +15,7 @@ function page() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-3 ">
             {cards.map((ele) => (
-              <div className="group relative p-5">
+              <div className="group relative p-5" key={ele.id}>
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:scale-105 sm:group-hover:scale-105 delay-105 duration-500 ease-linear	 ">
                   <Image
                     src={ele.image}
