1b66d3a1d04b30319344fc5c37cfaacb2a046dab
shoaib memon <shoaibattari83@gmail.com>
2023-10-28 16:25:51 +0500

    add

--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -10,10 +10,36 @@ function page() {
         <div className="mx-auto  px-6 lg:px-8">
           <div className=" mx-auto  lg:text-center">
             <p className="mt-2 text-5xl leading-loose font-bold  text-blue-900  sm:text-7xl">
-              All Subjects
+              All Subject
             </p>
           </div>
-         
+          <div className="grid grid-cols-1 md:grid-cols-3 ">
+            {cards.map((ele) => (
+              <div className="group relative p-5" key={ele.id}>
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:scale-105 sm:group-hover:scale-105 delay-105 duration-500 ease-linear	 ">
+                  <Image
+                    src={ele.image}
+                    alt={ele.subject}
+                    width={950}
+                    height={450}
+                    className="h-full w-full lg:h-full lg:w-full"
+                  />
+                </div>
+
+                <p className="text-1xl md:text-2xl   text-gray-900 mt-5  font-extrabold ">
+                  {ele.subject}
+                </p>
+
+                <div className="mt-4 flex justify-between md:min-h-[80px]">
+                  <div>
+                    <p className="text-1xl md:text-2xl  text-gray-900 mt-5  font-extrabold ">
+                      {ele.teacherName}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -13,7 +13,31 @@ function page() {
               All Teachers
             </p>
           </div>
-         
+          <div className="grid grid-cols-1 md:grid-cols-3 ">
+            {cards.map((ele) => (
+              <div className="group relative p-5" key={ele.id}>
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:scale-105 sm:group-hover:scale-105 delay-105 duration-500 ease-linear	 ">
+                  <Image
+                    src={ele.image}
+                    alt={ele.teacherName}
+                    width={950}
+                    height={450}
+                    className="h-full w-full lg:h-full lg:w-full"
+                  />
+                </div>
+                <p className="text-1xl md:text-2xl  text-gray-900 mt-5  font-extrabold ">
+                  {ele.teacherName}
+                </p>
+                <div className="mt-4 flex justify-between md:min-h-[80px]">
+                  <div>
+                    <p className="text-1xl   text-gray-900 mt-5  font-extrabold ">
+                      {ele.subject}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
