a042c5820f7883fad966e115a732368a20252bb1
shoaib memon <shoaibattari83@gmail.com>
2023-10-28 13:40:11 +0500

    DATABASE

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function page() {
   return (
     <div>All courses</div>
---
+++ src/app/database.tsx
@@ -0,0 +1,27 @@
+export const cards: {
+  id: number;
+  courseName: string;
+  teacherName: string;
+  subject: string;
+  duration: string;
+  image: string;
+}[] = [
+  {
+    id: 1,
+    courseName: "HTML",
+    teacherName: "SIR SALMAN:",
+    subject: "COMPUTER TECHNOLOGY",
+    duration: "1 MONTH",
+    image:
+      "https://media.geeksforgeeks.org/wp-content/cdn-uploads/20220630132824/HTML-Full-Form.jpg",
+  },
+  {
+    id: 2,
+    courseName: "FRONT END DEVELOPER",
+    teacherName: "SIR QASIM:",
+    subject: "COMPUTER TECHNOLOGY",
+    duration: "3 MONTH",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9BSm6eu42vnFlSt8F_ZinEkZgCOX6-hbKMlV9YIj2dTlQM-VcKpHUVVACdkT3EN7HUsg&usqp=CAU",
+  },
+];
